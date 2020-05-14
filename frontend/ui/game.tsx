28934d168638e02d7e4f895491db02ad22580217
David Edison <david.edison@hautelook.com>
2020-05-13 20:18:14 -0700

    change tag at bottom

--- frontend/ui/game.tsx
+++ frontend/ui/game.tsx
@@ -354,7 +354,7 @@ export class Game extends React.Component {
             Next game
           </button>
         </form>
-        <div id="coffee"><a href="https://www.buymeacoffee.com/jbowens" target="_blank">Buy the developer a coffee.</a></div>
+        <div id="coffee"><a href="http://moderntimesmerch.com" target="_blank">Buy the developer a beer.</a></div>
       </div>
     );
   }
